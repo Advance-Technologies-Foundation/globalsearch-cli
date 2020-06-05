415efb93b3f2bc524accffccbc012e2d71c86059
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-06-06 00:20:32 +0300

    docs

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,22 @@ import * as yosay from 'yosay';
 import * as inquirer from "inquirer";
 import * as setupGs from './user-workflow/installation';
 import * as healthCheckGs from './user-workflow/health-check';
+import * as supportingGs from './user-workflow/supporting';
 
 (async () => {
     try {
         const message = yosay(`Hello, I’m a global search helper assistant! Follow the instructions!`);
         console.log(message);
-        const setup = "Setup global search for creatio";
-        const check = "Check global search settings";
+        const setup = "Setup global search";
+        const check = "Check global search";
+        const supporting = "Supporting";
         let answer = await inquirer
             .prompt([
                 {
                     type: "list",
                     name: "action",
                     message: "What would you like to do?",
-                    choices: [setup, check],
+                    choices: [setup, check, supporting],
                     default: setup
                 }
             ]) as any;
@@ -25,6 +27,9 @@ import * as healthCheckGs from './user-workflow/health-check';
         }
         if (action === check) {
             await healthCheckGs.run();
+        }
+        if (action === supporting) {
+            await supportingGs.run();
         }
     } catch (e) {
         console.error(e);
